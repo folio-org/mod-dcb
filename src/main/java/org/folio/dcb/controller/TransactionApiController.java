85a9bca9dd93a0fd9ba4f7ff30f79878ec5f60b1
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-08-23 16:25:09 +0530

    UXPROD-4409 Initial Commit

--- src/main/java/org/folio/dcb/controller/TransactionApiController.java
+++ src/main/java/org/folio/dcb/controller/TransactionApiController.java
@@ -1,6 +1,7 @@
 package org.folio.dcb.controller;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import lombok.extern.log4j.Log4j2;
 import org.folio.dcb.rest.resource.TransactionsApi;
 import org.folio.dcb.domain.dto.TransactionStatusResponse;
 import org.springframework.http.HttpStatus;
@@ -9,14 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class TransactionApiController implements TransactionsApi {
 
   @Override
   public ResponseEntity<TransactionStatusResponse> getTransactionStatus(
-    @Parameter(name = "dcbTransactionId", description = "The ReShare DCB ID for the transaction being brokered.") @PathVariable("dcbTransactionId") String dcbTransactionId
-  ) {
-    TransactionStatusResponse response = new TransactionStatusResponse();
-    response.status(TransactionStatusResponse.StatusEnum.AWAITING_PICKUP);
-    return new ResponseEntity<>(response, HttpStatus.OK);
+    @Parameter(name = "dcbTransactionId", description = "The ReShare DCB ID for the transaction being brokered.")
+    @PathVariable("dcbTransactionId") String dcbTransactionId) {
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 }
