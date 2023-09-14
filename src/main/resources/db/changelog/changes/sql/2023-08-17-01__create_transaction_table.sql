421dcd6b3b06894ebcc0297aa6f2ff8bf20d1c80
Dmytro_Bykov <dmytro_bykov@epam.com>
2023-09-14 17:33:34 +0500

    Revert "MODDCB-1: Implement GET API for DCB Transaction"
    This reverts commit e679b64c4aa65d8ca622c5444f697adfac4af770.

--- src/main/resources/db/changelog/changes/sql/2023-08-17-01__create_transaction_table.sql
+++ src/main/resources/db/changelog/changes/sql/2023-08-17-01__create_transaction_table.sql
@@ -1,18 +1,20 @@
-create table transactions
-(
-    transaction_id         varchar not null primary key,
+CREATE TYPE StatusEnum as ENUM ('CREATED', 'OPEN', 'CANCELLED', 'IN_TRANSIT', 'AWAITING_PICKUP', 'ITEM_CHECKED_OUT', 'ITEM_CHECKED_IN', 'IN_TRANSIT_TO_LENDING', 'CLOSED', 'ERROR');
+CREATE CAST (character varying as StatusEnum) WITH INOUT AS IMPLICIT;
 
-    item_uuid              uuid    not null,
-    item_title             varchar not null,
-    item_barcode           varchar not null,
-    pickup_location        varchar not null,
-    material_type          varchar not null,
-    lending_library_code   varchar not null,
+CREATE TABLE IF NOT EXISTS transactions (
+    id UUID PRIMARY KEY,
 
-    patron_uuid            uuid    not null,
-    patron_group           varchar not null,
-    patron_barcode         varchar not null,
-    borrowing_library_code varchar not null,
+    item_id                UUID    NOT NULL,
+    item_title             VARCHAR NOT NULL,
+    item_barcode           VARCHAR NOT NULL,
+    pickup_location        VARCHAR NOT NULL,
+    material_type          VARCHAR NOT NULL,
+    lending_library_code   VARCHAR NOT NULL,
 
-    status                 varchar not null
+    patron_id              UUID    NOT NULL,
+    patron_group           VARCHAR NOT NULL,
+    patron_barcode         VARCHAR NOT NULL,
+    borrowing_library_code VARCHAR NOT NULL,
+
+    status                 StatusEnum
 );
