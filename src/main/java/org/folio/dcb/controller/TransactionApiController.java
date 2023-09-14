80cc8e64b0f057d9b0a355231f2cf7605712b278
Dmytro_Bykov <dmytro_bykov@epam.com>
2023-09-14 19:44:19 +0500

    MODDCB-1: GET API for DCB Transaction. RestControllerAdvice; annotations removed from overridden method of controller.

--- src/main/java/org/folio/dcb/controller/TransactionApiController.java
+++ src/main/java/org/folio/dcb/controller/TransactionApiController.java
@@ -1,6 +1,5 @@
 package org.folio.dcb.controller;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.dcb.mapper.TransactionStatusMapper;
@@ -9,7 +8,6 @@ import org.folio.dcb.domain.dto.TransactionStatusDto;
 import org.folio.dcb.service.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -22,9 +20,7 @@ public class TransactionApiController implements TransactionsApi {
   private final TransactionStatusMapper transactionStatusMapper;
 
   @Override
-  public ResponseEntity<TransactionStatusDto> getTransactionStatus(
-    @Parameter(name = "dcbTransactionId", description = "The ReShare DCB ID for the transaction being brokered.")
-    @PathVariable("dcbTransactionId") UUID dcbTransactionId) {
+  public ResponseEntity<TransactionStatusDto> getTransactionStatus(UUID dcbTransactionId) {
     return new ResponseEntity<>(transactionStatusMapper.mapToDto(transactionService.getTransactionStatusById(dcbTransactionId)), HttpStatus.OK);
   }
 }
--- src/main/java/org/folio/dcb/controller/advice/TransactionsExceptionHandler.java
+++ src/main/java/org/folio/dcb/controller/advice/TransactionsExceptionHandler.java
@@ -3,10 +3,10 @@ package org.folio.dcb.controller.advice;
 import org.folio.spring.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class TransactionsExceptionHandler {
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<String> handleNotFoundException(final NotFoundException e) {
