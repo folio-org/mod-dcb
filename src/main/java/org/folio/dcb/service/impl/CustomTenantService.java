c80124ae931fbd6b55ea75b7e016abe5b02b3e9b
Dmytro_Bykov <dmytro_bykov@epam.com>
2023-09-19 14:18:45 +0500

    MODDCB-17: System user for mod-dcb. TestTenant removed

--- src/main/java/org/folio/dcb/service/impl/CustomTenantService.java
+++ src/main/java/org/folio/dcb/service/impl/CustomTenantService.java
@@ -1,7 +1,6 @@
 package org.folio.dcb.service.impl;
 
 import lombok.extern.log4j.Log4j2;
-import org.folio.dcb.config.props.TestTenant;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
@@ -19,23 +18,19 @@ import org.springframework.stereotype.Service;
 public class CustomTenantService extends TenantService {
 
   private final PrepareSystemUserService systemUserService;
-  private final TestTenant testTenant;
 
 
   public CustomTenantService(JdbcTemplate jdbcTemplate, FolioExecutionContext context,
-                             FolioSpringLiquibase folioSpringLiquibase, PrepareSystemUserService systemUserService, TestTenant testTenant) {
+                             FolioSpringLiquibase folioSpringLiquibase, PrepareSystemUserService systemUserService) {
     super(jdbcTemplate, context, folioSpringLiquibase);
 
     this.systemUserService = systemUserService;
-    this.testTenant = testTenant;
   }
 
   @Override
   protected void afterTenantUpdate(TenantAttributes tenantAttributes) {
     log.debug("afterTenantUpdate:: parameters tenantAttributes: {}", tenantAttributes);
-    if (!context.getTenantId().startsWith(testTenant.getTenantName())) {
-      systemUserService.setupSystemUser();
-    }
+    systemUserService.setupSystemUser();
   }
 
 }
--- src/test/java/org/folio/dcb/service/CustomTenantServiceTest.java
+++ src/test/java/org/folio/dcb/service/CustomTenantServiceTest.java
@@ -1,56 +1,26 @@
 package org.folio.dcb.service;
 
-import org.folio.dcb.config.props.TestTenant;
 import org.folio.dcb.service.impl.CustomTenantService;
-import org.folio.spring.FolioExecutionContext;
-import org.folio.spring.FolioModuleMetadata;
 import org.folio.spring.service.PrepareSystemUserService;
 import org.folio.tenant.domain.dto.TenantAttributes;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import org.junit.jupiter.api.Test;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomTenantServiceTest {
 
-  private static final String TEST_TENANT = "test_tenant";
-  private static final String TENANT = "tenant";
-  private static final String TENANT_SCHEMA = "db_tenant";
-
   @Mock
   private PrepareSystemUserService systemUserService;
-  @Mock
-  private FolioExecutionContext context;
-  @Mock
-  private TestTenant testTenant;
-  @Mock
-  private FolioModuleMetadata moduleMetadata;
   @InjectMocks
   private CustomTenantService service;
   @Test
   void shouldPrepareSystemUser() {
-    mockTenantName(TENANT);
-//    mockTenantSchemaName();
-
     service.createOrUpdateTenant(new TenantAttributes());
-
     verify(systemUserService).setupSystemUser();
-  }
-
-  private void mockTenantName(String name) {
-    when(context.getTenantId()).thenReturn(name);
-    when(testTenant.getTenantName()).thenReturn(TEST_TENANT);
-  }
-
-  private void mockTenantSchemaName() {
-    when(context.getFolioModuleMetadata()).thenReturn(moduleMetadata);
-    when(moduleMetadata.getDBSchemaName(any())).thenReturn(TENANT_SCHEMA);
   }
 }
