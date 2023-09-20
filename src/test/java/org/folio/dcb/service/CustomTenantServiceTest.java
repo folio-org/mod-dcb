0770fa2e007de4c63c77997675c031da2a6f43e9
Dmytro_Bykov <dmytro_bykov@epam.com>
2023-09-20 14:42:53 +0500

    MODDCB-17: System user for mod-dcb. code smell elimination

--- src/test/java/org/folio/dcb/service/CustomTenantServiceTest.java
+++ src/test/java/org/folio/dcb/service/CustomTenantServiceTest.java
@@ -12,7 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class CustomTenantServiceTest {
+class CustomTenantServiceTest {
 
   @Mock
   private PrepareSystemUserService systemUserService;
