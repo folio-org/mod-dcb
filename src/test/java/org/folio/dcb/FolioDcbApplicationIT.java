00aaa878a6003a5b78a31cced1eeeba3d10f0b35
Dmytro_Bykov <dmytro_bykov@epam.com>
2023-11-03 18:58:18 +0500

    MODDCB-52: Secure setup of system users by default. JUnit 5

--- src/test/java/org/folio/dcb/FolioDcbApplicationIT.java
+++ src/test/java/org/folio/dcb/FolioDcbApplicationIT.java
@@ -1,70 +1,88 @@
 package org.folio.dcb;
 
 
-import org.folio.dcb.controller.BaseIT;
-import org.folio.dcb.repository.TransactionRepository;
-import org.folio.spring.service.SystemUserScopedExecutionService;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import org.folio.spring.integration.XOkapiHeaders;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.folio.dcb.domain.dto.DcbTransaction.RoleEnum.LENDER;
-import static org.folio.dcb.utils.EntityUtils.*;
-import static org.folio.dcb.utils.EntityUtils.createDcbTransactionByRole;
+import java.nio.file.Path;
+
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class FolioDcbApplicationIT extends BaseIT {
+@Testcontainers
+public class FolioDcbApplicationIT {
 
-  @Autowired
-  private TransactionRepository transactionRepository;
-  @Autowired
-  private SystemUserScopedExecutionService systemUserScopedExecutionService;
+  private static final Logger LOG = LoggerFactory.getLogger(FolioDcbApplicationIT.class);
+  /** Container logging, requires log4j-slf4j-impl in test scope */
+  private static final boolean IS_LOG_ENABLED = false;
+  protected static final String TOKEN = "test_token";
+  public static final String TENANT = "diku";
+  private static final Network NETWORK = Network.newNetwork();
 
-  @Test
-  public void health() throws Exception {
-    mockMvc.perform(
-        get("/admin/health")
-          .headers(defaultHeaders())
-          .contentType(MediaType.APPLICATION_JSON))
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.status").value("UP"));
+  @Container
+  public static final PostgreSQLContainer<?> POSTGRES =
+    new PostgreSQLContainer<>("postgres:12-alpine")
+      .withNetwork(NETWORK)
+      .withNetworkAliases("mypostgres")
+      .withExposedPorts(5432)
+      .withUsername("username")
+      .withPassword("password")
+      .withDatabaseName("postgres");
+
+  @Container
+  private static final GenericContainer<?> MOD_DCB =
+    new GenericContainer<>(
+      new ImageFromDockerfile("mod-dcb").withFileFromPath(".", Path.of(".")))
+      .dependsOn(POSTGRES)
+      .withNetwork(NETWORK)
+      .withNetworkAliases("mod-dcb")
+      .withExposedPorts(8087);
+
+  @BeforeAll
+  static void beforeAll() {
+    RestAssured.reset();
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    RestAssured.baseURI = "http://" + MOD_DCB.getHost() + ":" + MOD_DCB.getFirstMappedPort();
+    if (IS_LOG_ENABLED) {
+      MOD_DCB.followOutput(new Slf4jLogConsumer(LOG).withSeparateOutputStreams());
+    }
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    RestAssured.requestSpecification = null;  // unset X-Okapi-Tenant etc.
   }
 
   @Test
-  void createLendingCirculationRequestSmokeTest() throws Exception {
-    removeExistedTransactionFromDbIfSoExists();
+  void health() {
+    RestAssured.requestSpecification = new RequestSpecBuilder()
+      .addHeader(XOkapiHeaders.URL, "http://mod-dcb:8087")  // returns 404 for all other APIs
+      .addHeader(XOkapiHeaders.TENANT, TENANT)
+      .addHeader(XOkapiHeaders.TOKEN, TOKEN)
+      .addHeader(XOkapiHeaders.USER_ID, "08d51c7a-0f36-4f3d-9e35-d285612a23df")
+      .setContentType(ContentType.JSON)
+      .build();
 
-    this.mockMvc.perform(
-        post("/transactions/" + DCB_TRANSACTION_ID)
-          .content(asJsonString(createDcbTransactionByRole(LENDER)))
-          .headers(defaultHeaders())
-          .contentType(MediaType.APPLICATION_JSON)
-          .accept(MediaType.APPLICATION_JSON))
-      .andExpect(status().isCreated())
-      .andExpect(jsonPath("$.status").value("CREATED"))
-      .andExpect(jsonPath("$.item").value(createDcbItem()))
-      .andExpect(jsonPath("$.patron").value(createDefaultDcbPatron()));
-
-    //Trying to create another transaction with same transaction id
-    this.mockMvc.perform(
-        post("/transactions/" + DCB_TRANSACTION_ID)
-          .content(asJsonString(createDcbTransactionByRole(LENDER)))
-          .headers(defaultHeaders())
-          .contentType(MediaType.APPLICATION_JSON)
-          .accept(MediaType.APPLICATION_JSON))
-      .andExpectAll(status().is4xxClientError(),
-        jsonPath("$.errors[0].code", is("DUPLICATE_ERROR")));
+    when().
+      get("/admin/health")
+      .then()
+      .statusCode(200)
+      .body(is("UP"))
+      .contentType(ContentType.JSON);
   }
 
-  private void removeExistedTransactionFromDbIfSoExists() {
-    systemUserScopedExecutionService.executeAsyncSystemUserScoped(TENANT, () -> {
-      if (transactionRepository.existsById(DCB_TRANSACTION_ID)){
-        transactionRepository.deleteById(DCB_TRANSACTION_ID);
-      }
-    });
-  }
 }
