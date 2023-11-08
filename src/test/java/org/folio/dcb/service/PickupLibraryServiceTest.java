c628e6fb8e706795e90f17d813108abcfb29ff76
Dmytro Bykov <dmytro_bykov@epam.com>
2023-11-08 11:35:18 +0500

    MODDCB-9: CREATED-> OPEN: Implement DCB transaction status transition… (#37)
    MODDCB-9: CREATED-> OPEN: Implement DCB transaction status transition (Pickup library)

---
+++ src/test/java/org/folio/dcb/service/PickupLibraryServiceTest.java
@@ -0,0 +1,47 @@
+package org.folio.dcb.service;
+
+import org.folio.dcb.domain.dto.TransactionStatus;
+import org.folio.dcb.domain.entity.TransactionEntity;
+import org.folio.dcb.repository.TransactionRepository;
+import org.folio.dcb.service.impl.CirculationServiceImpl;
+import org.folio.dcb.service.impl.PickupLibraryServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.folio.dcb.domain.dto.DcbTransaction.RoleEnum.PICKUP;
+import static org.folio.dcb.utils.EntityUtils.createTransactionEntity;
+
+import static org.folio.dcb.utils.EntityUtils.createTransactionStatus;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+@ExtendWith(MockitoExtension.class)
+class PickupLibraryServiceTest {
+
+  @InjectMocks
+  private PickupLibraryServiceImpl pickupLibraryService;
+  @Mock
+  private TransactionRepository transactionRepository;
+  @Mock
+  private CirculationServiceImpl circulationService;
+  @Test
+  void updateTransactionTestFromCreatedToOpen() {
+    var transactionEntity = createTransactionEntity();
+    transactionEntity.setStatus(TransactionStatus.StatusEnum.CREATED);
+    transactionEntity.setRole(PICKUP);
+
+    pickupLibraryService.updateTransactionStatus(transactionEntity, TransactionStatus.builder().status(TransactionStatus.StatusEnum.OPEN).build());
+    Mockito.verify(transactionRepository, times(1)).save(transactionEntity);
+  }
+
+  @Test
+  void updateTransactionWithWrongStatusTest() {
+    TransactionEntity transactionEntity = createTransactionEntity();
+    TransactionStatus transactionStatus = createTransactionStatus(TransactionStatus.StatusEnum.AWAITING_PICKUP);
+    assertThrows(IllegalArgumentException.class, () -> pickupLibraryService.updateTransactionStatus(transactionEntity, transactionStatus));
+  }
+
+}
