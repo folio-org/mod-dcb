f8e6b1119373c96832cdafa18fa041d0a6ef1289
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-11-10 08:45:20 +0530

    MODDCB-8 Implementing Post transaction API for pickup library

--- src/main/java/org/folio/dcb/utils/DCBConstants.java
+++ src/main/java/org/folio/dcb/utils/DCBConstants.java
@@ -12,9 +12,11 @@ public class DCBConstants {
   public static final String LOCATION_ID = "9d1b77e8-f02e-4b7f-b296-3f2042ddac54";
   public static final String SERVICE_POINT_ID = "9d1b77e8-f02e-4b7f-b296-3f2042ddac54";
   public static final String HOLDING_ID = "10cd3a5a-d36f-4c7a-bc4f-e1ae3cf820c9";
+  public static final String LOAN_TYPE_ID = "4dec5417-0765-4767-bed6-b363a2d7d4e2";
   public static final String INSTANCE_TITLE = "DCB_INSTANCE";
   public static final String SOURCE = "FOLIO";
   public static final String INSTANCE_TYPE_SOURCE = "local";
   public static final String NAME = "DCB";
   public static final String CODE = "000";
+  public static final String MATERIAL_TYPE_NAME_BOOK = "book";
 }
--- src/test/java/org/folio/dcb/service/PickupLibraryServiceTest.java
+++ src/test/java/org/folio/dcb/service/PickupLibraryServiceTest.java
@@ -1,6 +1,7 @@
 package org.folio.dcb.service;
 
 import org.folio.dcb.domain.dto.TransactionStatus;
+import org.folio.dcb.domain.dto.TransactionStatusResponse;
 import org.folio.dcb.domain.entity.TransactionEntity;
 import org.folio.dcb.repository.TransactionRepository;
 import org.folio.dcb.service.impl.CirculationServiceImpl;
@@ -13,11 +14,22 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.folio.dcb.domain.dto.DcbTransaction.RoleEnum.PICKUP;
+import static org.folio.dcb.utils.EntityUtils.DCB_TRANSACTION_ID;
+import static org.folio.dcb.utils.EntityUtils.PICKUP_SERVICE_POINT_ID;
+import static org.folio.dcb.utils.EntityUtils.createDcbItem;
+import static org.folio.dcb.utils.EntityUtils.createDcbTransactionByRole;
+import static org.folio.dcb.utils.EntityUtils.createDefaultDcbPatron;
 import static org.folio.dcb.utils.EntityUtils.createTransactionEntity;
-
 import static org.folio.dcb.utils.EntityUtils.createTransactionStatus;
+import static org.folio.dcb.utils.EntityUtils.createUser;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class PickupLibraryServiceTest {
 
@@ -26,7 +38,12 @@ class PickupLibraryServiceTest {
   @Mock
   private TransactionRepository transactionRepository;
   @Mock
-  private CirculationServiceImpl circulationService;
+  private UserService userService;
+  @Mock
+  private RequestService requestService;
+  @Mock
+  private CirculationItemService circulationItemService;
+
   @Test
   void updateTransactionTestFromCreatedToOpen() {
     var transactionEntity = createTransactionEntity();
@@ -42,6 +59,27 @@ class PickupLibraryServiceTest {
     TransactionEntity transactionEntity = createTransactionEntity();
     TransactionStatus transactionStatus = createTransactionStatus(TransactionStatus.StatusEnum.AWAITING_PICKUP);
     assertThrows(IllegalArgumentException.class, () -> pickupLibraryService.updateTransactionStatus(transactionEntity, transactionStatus));
+  }
+
+  @Test
+  void createTransactionTest() {
+    var item = createDcbItem();
+    var patron = createDefaultDcbPatron();
+    var user = createUser();
+
+    when(userService.fetchOrCreateUser(any()))
+      .thenReturn(user);
+    doNothing().when(requestService).createHoldItemRequest(any(), any(), any());
+    doNothing().when(circulationItemService).checkIfItemExistsAndCreate(any(), any());
+
+    var response = pickupLibraryService.createCirculation(DCB_TRANSACTION_ID, createDcbTransactionByRole(PICKUP), PICKUP_SERVICE_POINT_ID);
+    verify(userService).fetchOrCreateUser(patron);
+    verify(circulationItemService).checkIfItemExistsAndCreate(item, PICKUP_SERVICE_POINT_ID);
+    verify(requestService).createHoldItemRequest(user, item, PICKUP_SERVICE_POINT_ID);
+
+    assertEquals(TransactionStatusResponse.StatusEnum.CREATED, response.getStatus());
+    assertEquals(item, response.getItem());
+    assertEquals(patron, response.getPatron());
   }
 
 }
