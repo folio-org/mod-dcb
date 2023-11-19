fa4d873af5bac6f5dc94ae2a64114ce27fc57b64
MagzhanArtykov <magzhan_artykov@epam.com>
2023-11-19 18:29:05 +0600

    MODDCB-49 Added unit tests

--- src/test/java/org/folio/dcb/service/CirculationRequestServiceTests.java
+++ src/test/java/org/folio/dcb/service/CirculationRequestServiceTests.java
@@ -19,7 +19,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class CirculationRequestServiceTests {
+class CirculationRequestServiceTests {
 
   @InjectMocks
   private CirculationRequestServiceImpl circulationRequestService;
