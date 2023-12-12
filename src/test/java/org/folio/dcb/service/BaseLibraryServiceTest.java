545bfd92d8038d13f3423c030369e72d1e6dc943
Adesh_Singh <Adesh_Singh@epam.com>
2023-12-12 15:05:29 +0530

    [MODDCB-82]-removed validations

--- src/test/java/org/folio/dcb/service/BaseLibraryServiceTest.java
+++ src/test/java/org/folio/dcb/service/BaseLibraryServiceTest.java
@@ -1,8 +1,6 @@
 
 package org.folio.dcb.service;
 
-import org.folio.dcb.domain.dto.CirculationItemRequest;
-import org.folio.dcb.domain.dto.ItemStatus;
 import org.folio.dcb.domain.dto.TransactionStatus;
 import org.folio.dcb.domain.dto.TransactionStatusResponse;
 import org.folio.dcb.domain.entity.TransactionEntity;
@@ -16,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
