308262c6963e3d69d6114d5bf13a82ce9b723094
MagzhanArtykov <magzhan_artykov@epam.com>
2024-03-07 23:35:00 +0600

    MODDCB-90 Accept existing circulation request ID
    (cherry picked from commit 3ad31891601c866ce74ee8591fff17d8c6d685ad)

--- src/test/java/org/folio/dcb/utils/EntityUtils.java
+++ src/test/java/org/folio/dcb/utils/EntityUtils.java
@@ -60,6 +60,7 @@ public class EntityUtils {
   public static String DCB_TYPE_USER_ID = "910c512c-ebc5-40c6-96a5-a20bfd81e154";
   public static String EXISTED_INVENTORY_ITEM_BARCODE = "INVENTORY_ITEM";
   public static String PATRON_TYPE_USER_ID = "18c1741d-e678-4c8e-9fe7-cfaeefab5eea";
+  public static String REQUEST_ID = "398501a2-5c97-4ba6-9ee7-d1cd6433cb98";
 
   public static DcbTransaction createDcbTransactionByRole(DcbTransaction.RoleEnum role) {
     return DcbTransaction.builder()
@@ -69,6 +70,14 @@ public class EntityUtils {
         default -> createDefaultDcbPatron();
         }
       )
+      .role(role)
+      .pickup(createDcbPickup())
+      .build();
+  }
+
+  public static DcbTransaction createEcsRequestTransactionByRole(DcbTransaction.RoleEnum role) {
+    return DcbTransaction.builder()
+      .requestId(REQUEST_ID)
       .role(role)
       .pickup(createDcbPickup())
       .build();
