15e2b89e99b2bcee4a3d8ea79abbc990472b7a4e
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2024-04-24 18:01:36 +0530

    MODDCB-98 Fetch the record from audit table so that the response of the API is immutable

--- src/main/java/org/folio/dcb/service/impl/TransactionsServiceImpl.java
+++ src/main/java/org/folio/dcb/service/impl/TransactionsServiceImpl.java
@@ -94,7 +94,7 @@ public class TransactionsServiceImpl implements TransactionsService {
   public TransactionStatusResponseCollection getTransactionStatusList(OffsetDateTime fromDate, OffsetDateTime toDate, Integer pageNumber, Integer pageSize) {
     log.info("getTransactionStatusList:: fromDate {}, toDate {}, pageNumber {}, pageSize {}",
       fromDate, toDate, pageNumber, pageSize);
-    var pageable = PageRequest.of(pageNumber, pageSize, Sort.by("updated_Date"));
+    var pageable = PageRequest.of(pageNumber, pageSize, Sort.by("created_Date"));
     var transactionAuditEntityPage= transactionAuditRepository.findTransactionsByDateRange(fromDate, toDate, pageable);
     var transactionStatusResponseList= transactionMapper.mapToDto(transactionAuditEntityPage);
     return TransactionStatusResponseCollection
