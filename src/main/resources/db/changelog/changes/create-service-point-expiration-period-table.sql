725b21582fb1ed4bd21e2e6288556b1c285af6a1
Antony_Hruschev <anton.gitgit@gmail.com>
2025-02-07 14:05:19 +0400

    UXPROD-5090 refactored table

--- src/main/resources/db/changelog/changes/create-service-point-expiration-period-table.sql
+++ src/main/resources/db/changelog/changes/create-service-point-expiration-period-table.sql
@@ -2,7 +2,7 @@ DROP TYPE IF EXISTS interval_id;
 CREATE TYPE interval_id AS ENUM ('Minutes', 'Hours', 'Days', 'Weeks', 'Months');
 CREATE TABLE IF NOT EXISTS service_point_expiration_period
 (
-    id uuid NOT NULL,
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
     duration integer NOT NULL,
     interval_id interval_id NOT NULL,
     CONSTRAINT service_point_expiration_period_pkey PRIMARY KEY (id)
