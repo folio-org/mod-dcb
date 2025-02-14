5d5c17307baba7652dda9b597a0eda67d2855cd9
Kapil Verma <85541312+kapil-epam@users.noreply.github.com>
2025-02-14 14:58:45 +0530

    Cherry Pick MODDCB-173

--- src/main/java/org/folio/dcb/client/feign/CirculationClient.java
+++ src/main/java/org/folio/dcb/client/feign/CirculationClient.java
@@ -3,12 +3,15 @@ package org.folio.dcb.client.feign;
 import org.folio.dcb.domain.dto.CheckInRequest;
 import org.folio.dcb.domain.dto.CheckOutRequest;
 import org.folio.dcb.domain.dto.CirculationRequest;
+import org.folio.dcb.domain.dto.LoanCollection;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "circulation", configuration = FeignClientConfiguration.class)
 public interface CirculationClient {
@@ -24,4 +27,7 @@ public interface CirculationClient {
   @PutMapping("/requests/{requestId}")
   CirculationRequest updateRequest(@PathVariable("requestId") String requestId,
     @RequestBody CirculationRequest circulationRequest);
+
+  @GetMapping("/loans")
+  LoanCollection fetchLoanByQuery(@RequestParam("query") String query);
 }
