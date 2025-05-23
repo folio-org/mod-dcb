67967579094057987b6d5e5a81f6c9c93769fb05
Kapil Verma <kapil_verma1@epam.com>
2025-05-23 16:51:21 +0530

    MODDCB-199: fix liquibase checksum issue for create-service-point-expiration-period-table.xml

--- src/main/resources/db/changelog/changes/create-service-point-expiration-period-table.sql
+++ src/main/resources/db/changelog/changes/create-service-point-expiration-period-table.sql
@@ -1,6 +1,5 @@
 DROP TYPE IF EXISTS interval_id;
 CREATE TYPE interval_id AS ENUM ('Minutes', 'Hours', 'Days', 'Weeks', 'Months');
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE IF NOT EXISTS service_point_expiration_period
 (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
