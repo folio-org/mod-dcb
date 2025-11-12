a6212cdb92399b84cde1b62657a881cba99f1f1f
Pavel Filippov <pavel_filippov@epam.com>
2025-11-12 15:10:10 +0200

    MODDCB-226: Improve code coverage

---
+++ src/test/java/org/folio/dcb/domain/ResultListTest.java
@@ -0,0 +1,25 @@
+package org.folio.dcb.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import support.types.UnitTest;
+
+@UnitTest
+class ResultListTest {
+
+  @Test
+  void asSinglePage_positive_varargsInput() {
+    var result = ResultList.asSinglePage(1, 2, 3);
+    assertThat(result.getResult()).isEqualTo(List.of(1, 2, 3));
+    assertThat(result.getTotalRecords()).isEqualTo(3);
+  }
+
+  @Test
+  void asSinglePage_positive_listInput() {
+    var result = ResultList.asSinglePage(List.of(1, 2, 3));
+    assertThat(result.getResult()).isEqualTo(List.of(1, 2, 3));
+    assertThat(result.getTotalRecords()).isEqualTo(3);
+  }
+}
