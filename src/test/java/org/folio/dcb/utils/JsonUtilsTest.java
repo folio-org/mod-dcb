8e4b05147d15be0903964099d159a3546cb16e13
Pavel Filippov <pavel_filippov@epam.com>
2026-07-14 14:49:16 +0300

    chore(unit-tests): Update tests to follow codestyle

--- src/test/java/org/folio/dcb/utils/JsonUtilsTest.java
+++ src/test/java/org/folio/dcb/utils/JsonUtilsTest.java
@@ -1,16 +1,17 @@
 package org.folio.dcb.utils;
 
-import org.folio.dcb.support.types.UnitTest;
-import org.folio.dcb.domain.entity.TransactionEntity;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.folio.dcb.utils.JsonUtils.DESERIALIZATION_FAILURE;
+
+import org.folio.dcb.domain.entity.TransactionEntity;
+import org.folio.dcb.support.types.UnitTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @UnitTest
 class JsonUtilsTest {
 
-    @ParameterizedTest
+  @ParameterizedTest
   @ValueSource(strings = {
     "{invalid:json}",
     "",
@@ -18,17 +19,9 @@ class JsonUtilsTest {
   })
   void jsonToObjectShouldThrowIllegalArgumentExceptionWhenDeserializationFails(String invalidJson) {
     // TestMate-c47cc79d469f3ffa287a5a8d01a25847
-    // Given
-    Class<TransactionEntity> targetClass = TransactionEntity.class;
-    // When
-    // Then
+    var targetClass = TransactionEntity.class;
     assertThatThrownBy(() -> JsonUtils.jsonToObject(invalidJson, targetClass))
       .isInstanceOf(IllegalArgumentException.class)
-      .hasMessageStartingWith(DESERIALIZATION_FAILURE)
-      .satisfies(ex -> {
-        String message = ex.getMessage();
-        assert message != null;
-        assert message.length() > DESERIALIZATION_FAILURE.length();
-      });
+      .hasMessageStartingWith(DESERIALIZATION_FAILURE);
   }
 }
