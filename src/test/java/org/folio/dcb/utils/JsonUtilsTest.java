436fa412e727fd2c1ffc8674fa10f2baca7c1892
Pavel Filippov <pavel_filippov@epam.com>
2026-07-15 14:20:56 +0300

    fix(tests): Fix tests to follow general codestyle

--- src/test/java/org/folio/dcb/utils/JsonUtilsTest.java
+++ src/test/java/org/folio/dcb/utils/JsonUtilsTest.java
@@ -1,14 +1,14 @@
 package org.folio.dcb.utils;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.folio.dcb.utils.JsonUtils.DESERIALIZATION_FAILURE;
 
 import org.folio.dcb.domain.entity.TransactionEntity;
 import org.folio.dcb.support.types.UnitTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.Test;
 
 @UnitTest
 class JsonUtilsTest {
@@ -27,16 +27,15 @@ class JsonUtilsTest {
       .hasMessageStartingWith(DESERIALIZATION_FAILURE);
   }
 
-    @Test
+  @Test
   void jsonToObjectShouldReturnDeserializedObjectWhenJsonIsValid() {
     // TestMate-84274ea70590cf0089b37902cd75521b
-    // Given
     var transactionId = "550e8400-e29b-41d4-a716-446655440000";
     var jsonString = "{\"id\":\"%s\"}".formatted(transactionId);
     var targetClass = TransactionEntity.class;
-    // When
+
     var result = JsonUtils.jsonToObject(jsonString, targetClass);
-    // Then
+
     assertThat(result.getId()).isEqualTo(transactionId);
   }
 }
