5ea55c2890b863a77d6787b65b0dd6336094ebe1
Pavel Filippov <pavel_filippov@epam.com>
2026-07-15 14:42:55 +0300

    fix(tests): Fix tests to follow general codestyle

--- src/test/java/org/folio/dcb/domain/mapper/TransactionMapperTest.java
+++ src/test/java/org/folio/dcb/domain/mapper/TransactionMapperTest.java
@@ -1,49 +1,50 @@
 package org.folio.dcb.domain.mapper;
 
-import org.folio.dcb.support.types.UnitTest;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
 import org.folio.dcb.domain.dto.DcbItem;
 import org.folio.dcb.domain.dto.DcbPatron;
 import org.folio.dcb.domain.dto.DcbPickup;
 import org.folio.dcb.domain.dto.DcbTransaction;
-import org.folio.dcb.domain.entity.TransactionEntity;
+import org.folio.dcb.support.types.UnitTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @UnitTest
 @ExtendWith(MockitoExtension.class)
 class TransactionMapperTest {
 
-  @InjectMocks
-  private TransactionMapper transactionMapper;
+  @InjectMocks private TransactionMapper transactionMapper;
 
-    private static Stream<Arguments> provideIncompleteDcbTransactions() {
-    DcbItem validItem = DcbItem.builder().id("item-id").barcode("item-barcode").build();
-    DcbPatron validPatron = DcbPatron.builder().id("patron-id").barcode("patron-barcode").build();
-    DcbPickup validPickup = DcbPickup.builder().servicePointId("sp-id").libraryCode("lib-code").build();
-    return Stream.of(
-      Arguments.of((DcbTransaction) null),
-      Arguments.of(DcbTransaction.builder().item(null).patron(validPatron).pickup(validPickup).build()),
-      Arguments.of(DcbTransaction.builder().item(validItem).patron(null).pickup(validPickup).build()),
-      Arguments.of(DcbTransaction.builder().item(validItem).patron(validPatron).pickup(null).build())
-    );
-  }
-
-    @ParameterizedTest
+  @DisplayName("mapToEntity_parameterized")
+  @ParameterizedTest(name = "[{index}] {0}")
   @MethodSource("provideIncompleteDcbTransactions")
-  void testMapToEntityShouldReturnNullWhenRequiredNestedObjectsAreMissing(DcbTransaction dcbTransaction) {
+  void mapToEntity_parameterized(@SuppressWarnings("unused") String name,
+    DcbTransaction dcbTransaction) {
     // TestMate-b53b895f93dcfd0a7ea23b049adfc896
-    // Given
-    String transactionId = "550e8400-e29b-41d4-a716-446655440000";
-    // When
-    TransactionEntity result = transactionMapper.mapToEntity(transactionId, dcbTransaction);
-    // Then
+    var transactionId = "550e8400-e29b-41d4-a716-446655440000";
+    var result = transactionMapper.mapToEntity(transactionId, dcbTransaction);
     assertThat(result).isNull();
   }
 
+  private static Stream<Arguments> provideIncompleteDcbTransactions() {
+    var validItem = DcbItem.builder().id("item-id").barcode("item-barcode").build();
+    var validPatron = DcbPatron.builder().id("patron-id").barcode("patron-barcode").build();
+    var validPickup = DcbPickup.builder().servicePointId("sp-id").libraryCode("lib-code").build();
+    return Stream.of(
+      Arguments.of("null transaction", null),
+      Arguments.of("valid item & patron & pickup ",
+        DcbTransaction.builder().item(null).patron(validPatron).pickup(validPickup).build()),
+      Arguments.of("valid item & pickup & null patron",
+        DcbTransaction.builder().item(validItem).patron(null).pickup(validPickup).build()),
+      Arguments.of("valid item & patron & null pickup",
+        DcbTransaction.builder().item(validItem).patron(validPatron).pickup(null).build())
+    );
+  }
 }
